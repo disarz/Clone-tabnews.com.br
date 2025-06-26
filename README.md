c78ce7b1d6ddddb693bfc6f7786ff8d681ea2a50
João A. Disarz <108777460+disarz@users.noreply.github.com>
2025-06-26 03:45:18 +0000

    Adição e cofiguração do Prettier

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Clone-tabnews.com.br
+
 Um estudo de programação para o Curso.dev: Clone do site https://Tabnews.com.br
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home () {
-    return <h1>Preciso ir dormir. (Isso é um teste de deploy.)</h1>
+function Home() {
+  return <h1>Preciso ir dormir. (Isso é um teste de deploy.)</h1>;
 }
 
-export default Home
+export default Home;
